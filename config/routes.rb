54cc929ad2f2bb4db629f196535a4d82ff1f096f
Hugo-Vidal <hugo.f.vidal.r@gmail.com>
2019-06-26 19:01:38 -0400

    Set up, scaffold history, Devise, views and navbar

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :histories
+  root "histories#index"
+  get "histories/my_index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
